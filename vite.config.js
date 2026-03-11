9a9136f1aeff045fa2ea7facba66540fae2078df
Jempasam <samueldemont83@gmail.com>
2026-03-11 18:30:55 +0100

    final

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import path from "path";
 /** @type {import('vite').UserConfig} */
 export default {
   build: {
-    outDir: path.join(__dirname, "docs"),
+    outDir: path.join(__dirname, "docs")
   },
   base: "/SamSite/",
 };
